6ac1f49837dcb043114cb0ee6948da5448faaaf8
Alejandro Varela <AVARELA@rosariobus.com.ar>
2021-03-22 14:59:24 -0300

    simplificación del programa

--- tutos/netcore/efcore/PruebaReverseEng0/Program.cs
+++ tutos/netcore/efcore/PruebaReverseEng0/Program.cs
@@ -13,24 +13,20 @@ namespace PruebaReverseEng0
                 var logs = ctx.LogGpsSinCalculoAtrasoYexcesoVelocidads
                     .ToList()
                     .Take(100)
-                    .AsQueryable<LogGpsSinCalculoAtrasoYexcesoVelocidad>()
                 ;
 
-                var iq = (
-                    from x in ctx.LogGpsSinCalculoAtrasoYexcesoVelocidads
-                    select x
-                ).Take(100);
+                // var iq = (
+                //      from x in ctx.LogGpsSinCalculoAtrasoYexcesoVelocidads
+                //      select x
+                // ).Take(100);
 
                 foreach (var log in logs)
                 {
-                    Console.WriteLine($"línea: {log.CodLinea,-10} | bandera: {log.CodBandera,-15}");
+                    Console.WriteLine($"equipo: {log.NroEquipo,-10} línea: {log.CodLinea,-10} | bandera: {log.CodBandera,-15}");
                 }
 
-                var sql_iq = iq.ToQueryString();
-                Console.WriteLine("sql iq   = " + sql_iq);
-
-                var sql_logs = logs.ToQueryString();
-                Console.WriteLine("sql logs = " + sql_logs);
+                // var sql_iq = iq.ToQueryString();
+                // Console.WriteLine("sql iq   = " + sql_iq);
             }
         }
     }
