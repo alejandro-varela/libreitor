64e09ae576f8560ee013e1d07b88b8dbed0d0991
alejandro-varela <hav29a@gmail.com>
2021-03-23 11:00:40 -0300

    experimento para ver si usa el operador de IN de SQL

--- tutos/netcore/efcore/PruebaReverseEng0/Program.cs
+++ tutos/netcore/efcore/PruebaReverseEng0/Program.cs
@@ -18,23 +18,31 @@ namespace PruebaReverseEng0
 
             using (logsTecnobusContext ctx = new logsTecnobusContext(configuration))
             {
-                var logs = ctx.LogGpsSinCalculoAtrasoYexcesoVelocidads
-                    .ToList()
-                    .Take(100)
-                ;
+                // var logs = ctx.LogGpsSinCalculoAtrasoYexcesoVelocidads
+                //     .ToList()
+                //     .Take(100)
+                // ;
 
-                // var iq = (
-                //      from x in ctx.LogGpsSinCalculoAtrasoYexcesoVelocidads
-                //      select x
-                // ).Take(100);
+                var bandes = new int[] { 183, 344, 484, 677, 333 };
 
-                foreach (var log in logs)
+                var iq = (
+                    from x in ctx.LogGpsSinCalculoAtrasoYexcesoVelocidads
+                    where 
+                        bandes.Contains(x.CodBandera ?? -1) &&
+                        (x.NroEquipo ?? -1) >= 1000000 &&
+                        (x.NroEquipo ?? -1) <= 4000000 &&
+                        true
+                    select x
+                )
+                .Take(100);
+
+                foreach (var log in iq)
                 {
                     Console.WriteLine($"equipo: {log.NroEquipo,-10} línea: {log.CodLinea,-10} | bandera: {log.CodBandera,-15}");
                 }
 
-                // var sql_iq = iq.ToQueryString();
-                // Console.WriteLine("sql iq   = " + sql_iq);
+                var sql_iq = iq.ToQueryString();
+                Console.WriteLine("sql iq   = " + sql_iq);
             }
         }
     }
