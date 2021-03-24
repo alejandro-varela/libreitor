c43be5d62387dd8092e465e6b4232c917ab779d3
alejandro-varela <hav29a@gmail.com>
2021-03-24 16:48:36 -0300

    init

---
+++ tutos/netcore/logging/PruebaSerilog/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Serilog;
+using Serilog.Events;
+
+// lecturas interesantes
+// https://www.campusmvp.es/recursos/post/como-manejar-trazas-en-net-core-con-serilog.aspx
+// https://github.com/serilog/serilog/wiki/Configuration-Basics
+
+// dotnet add package Serilog --version 2.10.0
+// dotnet add package Serilog.Sinks.Console
+// dotnet add package Serilog.Sinks.File
+
+namespace PruebaSerilog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("log.txt", LogEventLevel.Error)
+                .CreateLogger()
+            ;
+
+            logger.Debug("Iniciando programa");
+            logger.Debug("Hello World!");
+            logger.Error("Ohh no");
+            logger.Fatal("Naaa!!!");
+        }
+    }
+}
+
