75e08492064785fa985abb9b1fe8276b0d11f5b3
alejandro-varela <hav29a@gmail.com>
2022-03-14 13:05:03 -0300

    sanitizo los puntos en información histórica (si es que se pide la básica)

--- qpa/LibQPA.Testing/InformacionHistorica.cs
+++ qpa/LibQPA.Testing/InformacionHistorica.cs
@@ -1,17 +1,21 @@
 ﻿using Comun;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LibQPA.Testing
 {
     public class InformacionHistorica
     {
+        public int GranularidadMetrosDistancia { get; set; } = 20;
+        public bool GranularidadTiempoHabilitada { get; set; } = false;
+
         public List<PuntoHistorico> PuntosCrudos { get; set; }
         public CreadorPartesHistoricas CreadorPartes { get; set; }
 
-        public IEnumerable<ParteHistorica> GetPartesHistoricas(bool sanitizacionBasica = true)
+        public IEnumerable<ParteHistorica> GetPartesHistoricas(bool habilitarSanitizacionBasica = true)
         {
-            var puntos = sanitizacionBasica ? Sanitizar(PuntosCrudos) : PuntosCrudos;
+            var puntos = habilitarSanitizacionBasica ? Sanitizar(PuntosCrudos) : PuntosCrudos;
 
             foreach (var parteHistorica in CreadorPartes.Crear(puntos))
             {
@@ -21,7 +25,12 @@ namespace LibQPA.Testing
 
         List<PuntoHistorico> Sanitizar(List<PuntoHistorico> puntosCrudos)
         {
-            return puntosCrudos;
+            return puntosCrudos
+                .Where          (p => p.Lat != 0 && p.Lng != 0)
+                .OrderBy        (p => p.Fecha)
+                .HacerGranular  (GranularidadMetrosDistancia, GranularidadTiempoHabilitada)
+                .ToList         ()
+            ;
         }
     }
 }
