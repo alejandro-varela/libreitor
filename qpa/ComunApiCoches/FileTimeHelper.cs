4a3931922f0b17fd7e7d7feba7d726c74391d8d6
alejandro-varela <hav29a@gmail.com>
2022-11-08 12:31:31 -0300

    agregué un método para obtener la fecha desde el nombre de archivo

--- qpa/ComunApiCoches/FileTimeHelper.cs
+++ qpa/ComunApiCoches/FileTimeHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ComunApiCoches
 {
@@ -60,6 +62,51 @@ namespace ComunApiCoches
             {
                 return new Tuple<bool, Exception>(false, exx);
             }
+        }
+
+        public static (bool, DateTime) GetFileDameTime(string fileName)
+        {
+            var regExpHora = new Regex("^h[0-9]{2}");
+            var regExpDia  = new Regex("^d[0-9]{2}$");
+            var regExpMes  = new Regex("^mo[0-9]{2}$");
+            var regExpAnio = new Regex("^y[0-9]{4}$");
+
+            var partes = fileName.Split('/', '\\');
+            var hora = -1;
+            var dia = -1;
+            var mes = -1;
+            var anio = -1;
+
+            for (int i = partes.Length - 1; i >= 0; i--)
+            {
+                if (partes[i].StartsWith("h") && regExpHora.IsMatch(partes[i]))
+                {
+                    hora = int.Parse(partes[i].Substring(1, 2));
+                }
+
+                if (partes[i].StartsWith("d") && regExpDia.IsMatch(partes[i]))
+                {
+                    dia = int.Parse(partes[i].Substring(1));
+                }
+
+                if (partes[i].StartsWith("mo") && regExpMes.IsMatch(partes[i]))
+                {
+                    mes = int.Parse(partes[i].Substring(2));
+                }
+
+                if (partes[i].StartsWith("y") && regExpAnio.IsMatch(partes[i]))
+                {
+                    anio = int.Parse(partes[i].Substring(1));
+                    break;
+                }
+            }
+
+            if (anio == -1 || mes == -1 || dia == -1 || hora == -1)
+            {
+                return (false, DateTime.MinValue);
+            }
+
+            return (true, new DateTime(anio, mes, dia, hora, 0, 0));
         }
     }
 }
