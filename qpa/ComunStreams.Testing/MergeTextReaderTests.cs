c2b4c1130184e58fff17092e64fcd79423cb4620
alejandro-varela <hav29a@gmail.com>
2022-11-13 21:10:48 -0300

    agrego proyecto para testear ComunStreams

---
+++ qpa/ComunStreams.Testing/MergeTextReaderTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace ComunStreams.Testing
+{
+    public class MergeTextReaderTests
+    {
+        [Fact]
+        public void Prueba1()
+        {
+            var textReader1 = File.OpenText("./Data/a.txt");
+            var textReader2 = File.OpenText("./Data/b.txt");
+
+            List<TextReader> textReaders = new List<TextReader> { 
+                textReader1,
+                textReader2 
+            };
+
+            Func<string, string> fnSelectorOrden = (sRenglon) =>
+            {
+                return sRenglon;
+            };
+
+            var mergeTextReader = new MergeTextReader<string>(
+                textReaders,
+                fnSelectorOrden
+            );
+
+            for (; ; )
+            {
+                var sRenglon = mergeTextReader.ReadLine();
+
+                if (sRenglon == null)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
